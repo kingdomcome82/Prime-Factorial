31f9914d1c2f48dabb17f594b535bce9962cf1ba
kingdomCome <40040615+kingdomcome82@users.noreply.github.com>
2018-07-05 21:24:31 +0100

    functionally complete files

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function getPrimes() {
-  console.log('working');
+  var input = document.querySelector('input');
+  var display = document.querySelector('#displayParagraph');
+  var primes = [];
+
+  var inputNumber = input.value;
+
+  for (i=2;i<inputNumber;i++) {
+    if(inputNumber%i === 0) {
+      primes.push(i);
+      inputNumber = inputNumber/i;
+      i=1;
+    }
+  }
+
+
+  primes.push(inputNumber);
+  display.innerHTML = primes;
 }
